3e02731ff578eae060ef1fd5fb9134f3baaca991
Swapratim <Swapratim@users.noreply.github.com>
2017-02-12 19:50:50 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def makeYqlQuery(req):
     if city is None:
         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
@@ -69,19 +69,19 @@ def makeWebhookResult(data):
         return {}
 
     item = channel.get('item')
-	location = channel.get('location')
+    location = channel.get('location')
     units = channel.get('units')
-    if (location is None) or (item is None) or (units is None) or (atmosphere is None):
+    if (location is None) or (item is None) or (units is None):
         return {}
 
     condition = item.get('condition')
     if condition is None:
         return {}
 
-    #print(json.dumps(atmosphere, indent=1))
+    # print(json.dumps(item, indent=4))
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", temperature is " + condition.get('temp') + " " + units.get('temperature') 
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
