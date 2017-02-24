730c243cb6db6a1db8f8f1b62ff48d54d7fca634
Swapratim <Swapratim@users.noreply.github.com>
2017-02-24 14:24:35 +0100

    Add files via upload

--- context.py
+++ context.py
@@ -21,8 +21,7 @@ def webhook():
 
     print("Request:")
     print(json.dumps(reqContext, indent=4))
-    pritable_item = "*******ACTION*******" + reqContext.get("result").get("action")
-	print(pritable_item)
+    print("*******ACTION*******" + reqContext.get("result").get("action"))
     if reqContext.get("result").get("action") == "yahooWeatherForecast":
         app.py
         print ("Redirection to yahooWeatherForecast")
