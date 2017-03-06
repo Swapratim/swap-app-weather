0dc9af1eba0722861b99c9133d78ea75464c4e1a
Swapratim <Swapratim@users.noreply.github.com>
2017-03-06 11:10:33 +0100

    Add files via upload

--- context.py
+++ context.py
@@ -40,4 +40,4 @@ if __name__ == '__main__':
 
     print("Starting app on port %d" % port)
 
-    context.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
