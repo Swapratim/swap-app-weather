8671ca80ef10007aeee92b66759a455bfa67b42f
Swapratim <Swapratim@users.noreply.github.com>
2017-04-23 08:32:51 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -98,9 +98,10 @@ def searchhook():
     ###########################################################
     result = req.get("result")
     parameters = result.get("parameters")
-    search_array = parameters.get("any")
+    search_list0 = parameters.get("any")
     search_u_string_removed = [str(i) for i in search_array]
-    search_string = str(search_u_string_removed)
+    search_list1 = str(search_u_string_removed)
+    search_string = ''.join(str(e) for e in search_list1)
     print(search_string)
     if search_string is None:
         return None
