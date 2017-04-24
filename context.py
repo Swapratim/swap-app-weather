ca9cb438cdc3485d0fe1514b5a9bcc4781cf7b85
Swapratim <Swapratim@users.noreply.github.com>
2017-04-24 16:13:24 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -129,7 +129,7 @@ def searchhook():
     #    print items['snippet']
     #print(items)
 
-    ourResult = data['items'][5]['snippet']
+    ourResult = data['items'][0]['snippet']
     print(ourResult)
     #speech = items.get('snippet')
 
