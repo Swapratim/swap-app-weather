e0a2bc9ed58eba40294ad6b295525dc99e4725b0
Swapratim <Swapratim@users.noreply.github.com>
2017-05-23 08:48:35 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -41,6 +41,8 @@ def weatherhook():
     result = req.get("result")
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
+    print (city)
+    print ('********************')
     if city is None:
         return None
     yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
@@ -50,6 +52,7 @@ def weatherhook():
     yql_url = baseurl + urllib.parse.urlencode({'q': yql_query}) + "&format=json"
     result = urllib.request.urlopen(yql_url).read()
     data = json.loads(result)
+    print (data)
 ############################################################
     query = data.get('query')
     if query is None:
