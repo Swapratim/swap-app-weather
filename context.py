b6904ba80b48aaa35064f7abb58fafe6d6d21a8b
Swapratim <Swapratim@users.noreply.github.com>
2017-05-31 15:40:46 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -230,6 +230,7 @@ def dbsearchhook():
     return r
 
 def welcome():
+    print ("Within Welcome loop")
     data = request.json
     #sender = data['entry'][0]['messaging'][0]['sender']['id']
     #message = data['entry'][0]['messaging'][0]['message']['text']
@@ -242,6 +243,8 @@ def reply(user_id, msg):
         "recipient": {"id": user_id},
         "message": {"text": msg}
     }
+    print ("Data::::::::::::::::")
+    print (data)
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
