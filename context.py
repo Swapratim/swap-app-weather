5a5da2b47b404cf66d034125c5d8901a006c1a43
Swapratim <Swapratim@users.noreply.github.com>
2017-05-31 18:13:03 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -187,7 +187,14 @@ def searchhook():
                                  {
                                    "title" : "Donald Trump",
                                    "subtitle" : speech,
-                                   "image_url" : src_brace_removed_final
+                                   "image_url" : src_brace_removed_final,
+                                   "default_action": {
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                     },
                                 }
                             ]
                         }
