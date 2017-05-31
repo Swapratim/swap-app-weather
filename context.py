1deb776377c0bcf48a91f6e6b2699bf1d8e9da28
Swapratim <Swapratim@users.noreply.github.com>
2017-05-31 18:28:16 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -185,7 +185,7 @@ def searchhook():
                       "template_type" : "generic",
                        "elements" : [ 
                                  {
-                                   "title" : "Donald Trump",
+                                   "title" : " ",
                                    "subtitle" : speech,
                                    "image_url" : src_brace_removed_final,
                                 }
