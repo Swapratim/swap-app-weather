b6c1c0dc2408cc70603e41f69ffaa94fac0a7661
Swapratim <Swapratim@users.noreply.github.com>
2017-06-01 20:23:01 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -52,7 +52,7 @@ def welcome():
     first_name = data.get('first_name')
     print (first_name)
     #return "Hi"
-    speech = "You can ask about me about weather report of any city (like: what's the weather in Copenhagen) or search for an information (like: What is Game Of Thrones?)."
+    speech = "You can ask about me or weather of any city (like: what's the weather in Copenhagen) or search for an information (like: What is Game Of Thrones?)"
     res = {
           "speech": speech,
           "displayText": speech,
