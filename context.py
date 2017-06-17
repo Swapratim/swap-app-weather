3e7875699f732982499a87b82b82c35011cd3a2a
Swapratim <Swapratim@users.noreply.github.com>
2017-06-17 19:24:50 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -118,11 +118,11 @@ def weatherhook():
 
     if not parameters.get("geo-city"):
        city = parameters.get("geo-city-dk")
-       return city
+       #return city
 
     if not parameters.get("geo-city-dk"):
        city = parameters.get("geo-city")
-       return city
+       #return city
     print (city)
     print ('********************')
     #if city is None:
