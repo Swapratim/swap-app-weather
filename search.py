ce65169d2d820fb5d871a65b3d7b64fd2a6b1553
Swapratim <Swapratim@users.noreply.github.com>
2017-06-23 15:54:54 +0200

    Add files via upload

---
+++ search.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+import urllib.request, urllib.parse, urllib.error
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+from context.py import req
+
+# Flask app should start in global layout
+app = Flask(__name__)
+print ("Redirection lands to search.py")
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "GoogleSearch":
+        return {}
+    baseurl = "https://www.googleapis.com/customsearch/v1?"
+    google_query = makeSearchQuery(req)
+    if google_query is None:
+        return {}
+    google_url = baseurl + urllib.parse.urlencode({google_query})
+    print (google_url)
+    sys.stdout.flush()
+    result = urllib.request.urlopen(google_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeSearchQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    search_string = parameters.get("any")
+    print (search_string)
+    sys.stdout.flush()
+    #key = "AIzaSyDNYsLn4JGIR4UaZMFTAgDB9gKN3rty2aM"
+    #cse = "003066316917117435589%3Avcms6hy5lxs"
+    if search_string is None:
+        return None
+
+    return "key=AIzaSyDNYsLn4JGIR4UaZMFTAgDB9gKN3rty2aM&cx=003066316917117435589%3Avcms6hy5lxs&q='" + search_string + "'&num=1"
+
+
+def makeWebhookResult(data):
+    parsed_input = data.get('query')
+    if parsed_input is None:
+        return {}
+
+    items = parsed_input.get['items']
+    if items is None:
+        return {}
+
+    speech = items.get('snippet')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
---
+++ weather.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+import urllib.request, urllib.parse, urllib.error
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask weather should start in global layout
+weather = Flask(__name__)
+
+
+def weatherhook():
+    req = request.get_json(silent=True, force=True)
+    print("Request:::Swapratim")
+    print(json.dumps(req, indent=4))
+    res = processRequest(req)
+    print ("------First res:")
+    print (res)
+    res = json.dumps(res, indent=4)
+    print ("------Second res:")
+    print (res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'weatherlication/json'
+    print ("Printing the result::::::::::::")
+    print (r)
+    return r
+	
+	
+def processRequest(req):
+    print ("*******Within processRequest function within weather")
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urllib.parse.urlencode({'q': yql_query}) + "&format=json"
+    result = urllib.request.urlopen(yql_url).read()
+    data = json.loads(result)
+    print ("Before hitting makeWebhookResult function")
+    res = makeWebhookResult(data)
+    print (res)
+    return res
+
+
+def makeYqlQuery(req):
+    print ("Within makeYqlQuery function")
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    print ("makeWebhookResult -- Result is loaded")
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+#if __name__ == '__main__':
+ #   processRequest(req)
