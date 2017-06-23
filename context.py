74a4c9a1dd3901777ad1075295b749ac909b19d9
Swapratim <Swapratim@users.noreply.github.com>
2017-06-23 16:41:22 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -358,6 +358,7 @@ def searchhook():
     search_string_ascii = search_string.encode('ascii')
     if search_string_ascii is None:
         return None
+    search_string_ascii =  search_string.decode(encoding)
     google_query = "key=AIzaSyDNYsLn4JGIR4UaZMFTAgDB9gKN3rty2aM&cx=003066316917117435589%3Avcms6hy5lxs&q=" + search_string_ascii + "&num=1"
 ###########################################################
     if google_query is None:
